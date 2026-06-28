f80dd9c8d7049f0205348c269f627ea0fd793e17
Fhilly-Prime <bogdanfhilii@gmail.com>
2026-06-28 22:54:03 +0300

    День 1 - переменные и строки

---
+++ hello.py
@@ -0,0 +1,7 @@
+hours_per_day = 4
+minutes_per_day = hours_per_day * 60
+minutes_per_week = minutes_per_day * 7
+
+print("Часов в день:", hours_per_day)
+print("Минут в день:", minutes_per_day)
+print("Минут в неделю:", minutes_per_week)
---
+++ strings.py
@@ -0,0 +1,12 @@
+name = "богдан филли"
+
+print(name.upper())    # всё в верхний регистр
+print(name.lower())    # всё в нижний регистр
+print(name.title())    # каждое слово с большой буквы
+
+first_name = "Богдан"
+last_name = "Филли"
+full_name = first_name + " " + last_name
+
+print("Привет,", full_name)
+print("Привет, " + full_name + "!")
